2e723ee18df0a26bd2401af3dfc98de7e0c30dea
Crowdbotics <team@crowdbotics.com>
2023-05-02 19:30:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "purple-snow-40406",
+  url: "https://purple-snow-40406.botics.co",
+  api: "https://purple-snow-40406.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
